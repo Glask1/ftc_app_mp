e0a04e879384eb54e834166fb97a24d423c02430
G <gary@glask.info>
2020-01-05 22:09:00 -0500

    duplicate dependecies

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoONE.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoONE.java
@@ -3,14 +3,22 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
+
 import io.github.ensozos.core.MatrixProfile;
 
-@Autonomous(name="LeftMid")
+@Autonomous(name="MatrixProfile")
 public class AutoONE extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         MatrixProfile matrixProfile = new MatrixProfile();
         int window = 4;
+
+        INDArray target = Nd4j.create(new double[]{0.0, 6.0, -1.0, 2.0, 3.0, 1.0, 4.0}, new int[]{1, 7});
+        INDArray query = Nd4j.create(new double[]{1.0, 2.0, 0.0, 0.0, -1}, new int[]{1, 5});
+
+        matrixProfile.stamp(target, window, 1.0);
 
         while (!isStopRequested()){
             telemetry.update();
